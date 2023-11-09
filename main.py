06665321527a5efdd45325e98860f1d0b27e195b
Ju-S <wntjd0643@gmail.com>
2023-11-09 20:58:16 +0900

    Feat: 메인class생성 및 내부 로직 변경

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
-def error():
-    print("ERROR!")
-    return
+class Calculator():
+    operator_dic = {"+" : "plus", "-" : "minus", "*" : "multiply"}
 
-#TODO
-def calculate(n1, n2, op):
-    pass
+    selected_operator = None
 
-def calculator():
-    n1 = input()
-    op = input()
-    flag = False; # False 일 때가 숫자를 입력받아야 하는 상태임.
+    prev_input = None
+    current_input = None
 
-    if not n1.isdigit():
-        error()
-    if not (op =='*' or op=='-' or op=='+'):
-        error()
+    error_state = None
 
-    while True:
-        temp = input()
-        if not (temp.isdigit() or temp in ['*', '-', '+']): # 숫자가 아니거나, 연산자가 아니면 error
-            error()
-        else :
-            if temp.isdigit() and (not flag):
-                calculate(n1, temp, op)
-            elif flag and (temp == '*' or temp=='-' or temp == '+') and temp == op:
-                continue;
-            else :
-                error()
+    def __init__(self):
+        self.error_state = False
+        self.is_calculate_done = False
+        pass
+
+    def calculate(self, n1, n2, op):
+        pass
+
+    def errorCheck(self):
+        # TODO if prev_input is number then current_input must be operator, if prev_input is operator then current_input must be number
+
+        # TODO if current_input is number but is not integer then error_state is True
+
+        # TODO if current_input is operator but is not selected_operator then error_state is True
+
+        if self.error_state:
+            print("ERROR!")
+            return
+
+    def calculate_Start(self):
+        while True:
+            self.current_input = input()
+            self.errorCheck()
+            if self.is_calculate_done:
+                # TODO 결과값 출력
+                pass
+
+            #if not (temp.isdigit() or temp in ['*', '-', '+']): # 숫자가 아니거나, 연산자가 아니면 error
+                #error()
+            #else :
+                #if temp.isdigit() and (not flag):
+                    #calculate(slef, n1, temp, op)
+                #elif flag and (temp == '*' or temp=='-' or temp == '+') and temp == op:
+                    #continue
+                #else :
+                    #error()
 
 if __name__ == '__main__':
-    calculator()
-
+    calculator = Calculator()
+    calculator.calculate_Start()
