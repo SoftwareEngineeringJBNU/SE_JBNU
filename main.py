0d8ee13f9503609d723df29cfb613f7ed81d7ee7
DodoJBNU <dh723@jbnu.ac.kr>
2023-12-18 15:04:23 +0900

    Comment : 주석 해제

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from testCalculator import TestAddFunc, TestSubFunc, TestMulFunc, TestFacFunc
 import unittest
 
 if __name__ == '__main__':
-    #calculator = Calculator()
-    #calculator.calculate_Start()
-    unittest.main()
+    calculator = Calculator()
+    calculator.calculate_Start()
+    #unittest.main()
 
 
 
